f80a419c2c891f1fc4d00bce59ad757b86521bf0
mr4kobess <mansur.tumashev@gmail.com>
2022-05-17 20:16:04 +0500

    init

--- train_scoreboard/urls.py
+++ train_scoreboard/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -24,4 +26,4 @@ urlpatterns = [
     path(r'redoc-api/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('', include('train.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
