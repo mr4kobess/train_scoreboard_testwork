55ad3ddb8b457d825fd5bf1f5d9dafe4719e8801
mr4kobess <mansur.tumashev@gmail.com>
2022-05-18 12:56:21 +0500

    init

--- train_scoreboard/urls.py
+++ train_scoreboard/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
 
     path('', include('train.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
